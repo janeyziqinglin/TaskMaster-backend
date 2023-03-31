f99984863306f19a57761d729e2f80058fc45c8c
JaneyLin <105125897+janeyziqinglin@users.noreply.github.com>
2023-03-31 14:09:06 -0400

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors(
-{  origin: ["http://localhost:3000", "https://task-master-ojj3.onrender.com/",]}
+// {  origin: ["http://localhost:3000", "https://task-master-ojj3.onrender.com/",]}
 )); //cors need to be on before routes
 
 app.use("/api/tasks",taskRoutes);
